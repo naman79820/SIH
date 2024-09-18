e014424598ffd08357d4e4a00b22a974467b72b7
“naman79820” <naman79820@gmail.com>
2024-09-19 02:31:06 +0530

    homepage 60% done

--- src/app/components/body.jsx
+++ src/app/components/body.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useEffect, useRef, useState } from "react";
 import logo1 from "../util/body.jpg";
 import logo2 from "../util/3d.png";
 import logo3 from "../util/logo3.png";
@@ -20,6 +21,28 @@ const poppins = Poppins({
 });
 
 const Body = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div>
@@ -45,9 +68,133 @@ const Body = () => {
               <Image src={logo1} alt="Logo" width={750} height={150} />
             </div>
           </div>
-          <div></div>
         </div>
-        
+
+        <div
+          ref={ref}
+          className={`bg-[#04b442] w-[90vw] mx-auto rounded-3xl mt-10 pl-5 pb-20  transition-transform duration-500 ${
+            isVisible ? "zoom-in" : "zoom-out"
+          }`}
+        >
+          <div className="flex">
+            <div className={`pl-5 w-[40vw] text-white ${anton.className} `}>
+              <p className="text-white text-[70px] pt-16 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing
+              </p>
+
+              <p className={`${poppins.className} mt-16 w-[34vw] text-xl `}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+                voluptate ipsam velit et deleniti incidunt eum reiciendis
+                asperiores. Ab labore numquam ipsa incidunt cupiditate impedit.
+                Impedit rem nostrum consequuntur animi.
+              </p>
+            </div>
+            <div className="flex relative">
+              <div className="flex justify-between items-center">
+                <Image
+                  className="ml-32 object-contain rotated-image1 "
+                  src={logo3}
+                  alt="Logo"
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  className="object-contain pt-[450px] rotated-image2 rounded-none "
+                  src={logo3}
+                  alt="Logo"
+                  width={250}
+                  height={150}
+                />
+              </div>
+              <Image
+                className="object-contain rotating-image rounded-none absolute"
+                src={logo3}
+                alt="Logo"
+                width={250}
+                height={150}
+              />
+            </div>
+          </div>
+          <div className={`flex gap-10 justify-evenly ${anton.className}`}>
+          <div className="w-[25rem] h-[16rem] bg-[#BEED57] rounded-3xl flex items-center justify-around zoomable-div">
+  <p className="text-2xl w-[10vw]">Lorem ipsum dolor sit amet</p>
+  <p>
+    <a
+      className="inline-block rounded-full border border-black p-3 text-black hover:bg-black hover:black hover:text-white focus:outline-none focus:ring a"
+      href="#"
+    >
+      <span className="sr-only"> Download </span>
+      <svg
+        className="size-5 rtl:rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+        />
+      </svg>
+    </a>
+  </p>
+</div>
+            <div className="w-[25rem] h-[16rem] bg-[#BEED57] rounded-3xl flex items-center justify-around zoomable-div">
+  <p className="text-2xl w-[10vw]">Lorem ipsum dolor sit amet</p>
+  <p>
+    <a
+      className="inline-block rounded-full border border-black p-3 text-black hover:bg-black hover:black hover:text-white focus:outline-none focus:ring a"
+      href="#"
+    >
+      <span className="sr-only"> Download </span>
+      <svg
+        className="size-5 rtl:rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+        />
+      </svg>
+    </a>
+  </p>
+</div>
+
+<div className="w-[25rem] h-[16rem] bg-[#BEED57] rounded-3xl flex items-center justify-around zoomable-div">
+  <p className="text-2xl w-[10vw]">Lorem ipsum dolor sit amet</p>
+  <p>
+    <a
+      className="inline-block rounded-full border border-black p-3 text-black hover:bg-black hover:black hover:text-white focus:outline-none focus:ring a"
+      href="#"
+    >
+      <span className="sr-only"> Download </span>
+      <svg
+        className="size-5 rtl:rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+        />
+      </svg>
+    </a>
+  </p>
+</div>
+            {/* Repeat for other divs */}
+          </div>
+        </div>
       </div>
     </>
   );
